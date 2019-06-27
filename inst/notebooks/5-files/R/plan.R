5084793f434437f3304c0ea027de92910c7b0650
wlandau-lilly <will.landau@lilly.com>
2019-06-27 15:37:14 -0400

    Change word choice

--- inst/notebooks/5-files/R/plan.R
+++ inst/notebooks/5-files/R/plan.R
@@ -6,7 +6,7 @@ plan <- drake_plan(
     initial_split(prop = 0.3),
   rec = prepare_recipe(data),
   # Set `model_file = file_out("model.h5")` in the call to train_model().
-  # Name this target `history` instead of `model`.
+  # Name this target `progression` instead of `model`.
   model = train_model(data, rec),
   # Replace `model` with `file_in("model.h5")`:
   conf = confusion_matrix(data, rec, model),
