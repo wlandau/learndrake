567b863984cfed8affebe857e6bee989dfb6b882
wlandau-lilly <will.landau@lilly.com>
2019-07-24 09:06:24 -0400

    Repair slide deployment

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ echo "  TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
 echo "  TRAVIS_PULL_REQUEST_BRANCH: ${TRAVIS_PULL_REQUEST_BRANCH}"
 if [[ "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]
 then
-  echo "Deploying to binder."
+echo "Deploying to binder."
   git config --global user.email "will.landau@gmail.com"
   git config --global user.name "wlandau"
   git clone -b binder https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git binder
@@ -17,13 +17,15 @@ then
   git add *
   git commit -am "Update binder workspace" || true
   git push -q origin binder
+  cd ..
 
   echo "Deploying to gh-pages."
   git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
   cd gh-pages
   ls -a | grep -Ev "^\.$|^\.\.$|^\.git$" | xargs rm -rf
-  Rscript -e 'learndrake::save_slides(".", overwrite = TRUE)'
+  cp -r ../inst/slides/* ./
   git add *
   git commit -am "Update gh-pages slides" || true
   git push -q origin gh-pages
+  cd ..
 fi
