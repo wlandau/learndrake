88583dafad5322de16120ba644edd1039d895fa6
wlandau <will.landau@gmail.com>
2019-08-18 08:50:20 -0400

    Update the files chapter

--- inst/notebooks/5-files/R/plan.R
+++ inst/notebooks/5-files/R/plan.R
@@ -5,9 +5,10 @@ plan <- drake_plan(
   ) %>%
     initial_split(prop = 0.3),
   rec = prepare_recipe(data),
-  # Set `model_file = file_out("model.h5")` in the call to train_model().
-  # Name this target `progression` instead of `model`.
-  model = train_model(data, rec),
+  model = target( # Name this target `progression` instead of `model`.
+  	train_model(data, rec), # Set `model_file = file_out("model.h5")` in the call to train_model().
+  	format = "keras" # The target value is no longer a model, so remove this line with the format.
+  ),
   # Replace `model` with `file_in("model.h5")`:
   conf = confusion_matrix(data, rec, model),
   metrics = compare_models(conf)
