65087692bc2db3ff69f53abe06e478c05ab52385
wlandau-lilly <will.landau@lilly.com>
2019-09-01 16:49:14 -0600

    Edit deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ echo "  TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
 echo "  TRAVIS_PULL_REQUEST_BRANCH: ${TRAVIS_PULL_REQUEST_BRANCH}"
 if [[ "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]
 then
-echo "Deploying to binder."
+  echo "Deploying to binder."
   git config --global user.email "will.landau@gmail.com"
   git config --global user.name "wlandau"
   git clone -b binder https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git binder
