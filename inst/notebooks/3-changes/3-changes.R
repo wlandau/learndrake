05ac746e384b33204f3a9d91a781507be53a4afa
wlandau-lilly <will.landau@lilly.com>
2020-02-07 15:37:21 -0500

    Update 3-changes plan

---
+++ inst/notebooks/3-changes/3-changes.R
@@ -0,0 +1,8 @@
+# You may wish to write scratch work below as you complete the exercises.
+# Otherwise, use the 3-changes.Rmd notebook or the R console.
+
+# Setup:
+basename(getwd()) # Should be "3-changes"
+source("../config/options.R")
+
+# Write your code below.
