cadbef2fac28bb68d269668bc3dec5a3bb1c66cd
wlandau <will.landau@gmail.com>
2020-02-09 16:16:34 -0500

    Add refs to static branching app

---
+++ inst/notebooks/4-static/4-static.R
@@ -0,0 +1,9 @@
+# Go to <https://wlandau.shinyapps.io/learndrakestatic>
+# and complete the exercises.
+# The app at <https://wlandau.shinyapps.io/drakeplanner> will help you.
+# You can also use this script for scratch work.
+
+library(drake)
+# Write your plans here
+# plan <- drake_plan(...)
+# vis_drake_graph(plan)
