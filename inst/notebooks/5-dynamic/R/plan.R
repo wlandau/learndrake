be2f49fd1e405beccb66327e49409236f23ead77
wlandau-lilly <will.landau@lilly.com>
2020-02-10 10:06:03 -0500

    Start dynamic chapter

--- inst/notebooks/5-dynamic/R/plan.R
+++ inst/notebooks/5-dynamic/R/plan.R
@@ -4,7 +4,7 @@ plan <- drake_plan(
   run_relu = test_model(act1 = "relu", churn_data, churn_recipe),
   run_sigmoid = test_model(act1 = "sigmoid", churn_data, churn_recipe),
   run_softmax = test_model(act1 = "softmax", churn_data, churn_recipe),
-  best_run = rbind(run_relu, run_sigmoid, run_softmax) %>%
+  best_run = bind_rows(run_relu, run_sigmoid, run_softmax) %>%
     filter(accuracy == max(accuracy)),
   best_model = target(
     train_best_model(best_run, churn_recipe),
