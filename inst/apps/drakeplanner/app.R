5cccdf78ce91d9387fc1183cd715b8ede46b444b
wlandau <will.landau@gmail.com>
2020-02-10 18:02:08 -0500

    Tryu to fix bug

--- inst/apps/drakeplanner/app.R
+++
@@ -1,1 +0,0 @@
-drakeplanner::drakeplanner()
--- inst/apps/drakeplanner/tests/open.R
+++
@@ -1,8 +0,0 @@
-# nolint start
-app <- ShinyDriver$new("../", seed = 0)
-app$snapshotInit("open")
-
-app$setInputs(table_rows_current = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), allowInputNoBinding_ = TRUE)
-app$setInputs(table_rows_all = c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14), allowInputNoBinding_ = TRUE)
-app$snapshot()
-# nolint end
