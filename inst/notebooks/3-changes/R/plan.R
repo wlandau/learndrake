8bd49f778036c653b3cd194d5e764b9c0252df86
wlandau <will.landau@gmail.com>
2020-02-10 07:20:52 -0500

    Use tibble printing

--- inst/notebooks/3-changes/R/plan.R
+++ inst/notebooks/3-changes/R/plan.R
@@ -1,6 +1,6 @@
 plan <- drake_plan(
   churn_data = split_data(file_in("../data/customer_churn.csv")),
-  churn_recipe = prepare_recipe(churn_data), 
+  churn_recipe = prepare_recipe(churn_data),
   run_relu = test_model(act1 = "relu", churn_data, churn_recipe),
   run_sigmoid = test_model(act1 = "sigmoid", churn_data, churn_recipe),
   run_softmax = test_model(act1 = "softmax", churn_data, churn_recipe),
