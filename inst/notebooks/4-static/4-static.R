98ead538efbf413a57757a495670001b3176a527
wlandau-lilly <will.landau@lilly.com>
2020-02-10 09:40:56 -0500

    Add link

--- inst/notebooks/4-static/4-static.R
+++ inst/notebooks/4-static/4-static.R
@@ -2,6 +2,8 @@
 # and complete the exercises.
 # The app at <https://wlandau.shinyapps.io/drakeplanner> will help you.
 # You can also use this script for scratch work.
+# For more information on static branching,
+# visit <https://books.ropensci.org/drake/static.html>.
 
 library(drake)
 # Write your plans here
