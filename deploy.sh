927240b1118740a42e636cbde35a2436326a6fe7
wlandau-lilly <will.landau@lilly.com>
2020-02-14 10:26:11 -0500

    Re-automate deployment of slides

--- deploy.sh
+++ deploy.sh
@@ -7,18 +7,6 @@ echo "  TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
 echo "  TRAVIS_PULL_REQUEST_BRANCH: ${TRAVIS_PULL_REQUEST_BRANCH}"
 if [[ "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]
 then
-  echo "Deploying to binder."
-  git config --global user.email "will.landau@gmail.com"
-  git config --global user.name "wlandau"
-  git clone -b binder https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git binder
-  cd binder
-  ls -a | grep -Ev "^\.$|^\.\.$|^\.git$|^\.binder$" | xargs rm -rf
-  cp -r ../inst/notebooks/* ./
-  git add *
-  git commit -am "Update binder workspace" || true
-  git push -q origin binder
-  cd ..
-
   echo "Deploying to gh-pages."
   git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
   cd gh-pages
@@ -28,7 +16,19 @@ then
   git commit -am "Update gh-pages slides" || true
   git push -q origin gh-pages
   cd ..
-  
-  echo "Deploying to shinyapps.io."
-  Rscript shinyapps.R
+
+#  echo "Deploying to binder."
+#  git config --global user.email "will.landau@gmail.com"
+#  git config --global user.name "wlandau"
+#  git clone -b binder https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git binder
+#  cd binder
+#  ls -a | grep -Ev "^\.$|^\.\.$|^\.git$|^\.binder$" | xargs rm -rf
+#  cp -r ../inst/notebooks/* ./
+#  git add *
+#  git commit -am "Update binder workspace" || true
+#  git push -q origin binder
+#  cd ..
+#  
+#  echo "Deploying to shinyapps.io."
+#  Rscript shinyapps.R
 fi
